ba4b7698a7b9ee2019f27c26048b15381213e249
Mark Toda <toda.mark@gmail.com>
2022-11-07 15:53:08 -0800

    fix: lint

--- tests/forge/SwapGenieCallParameters.t.sol
+++ tests/forge/SwapGenieCallParameters.t.sol
@@ -167,7 +167,7 @@ contract SwapGenieCallParametersTest is Test, Interop, DeployRouter {
 
         require(success, "call failed");
         assertEq(token.balanceOf(RECIPIENT), 3);
-        assertEq(from.balance, balance-params.value);
+        assertEq(from.balance, balance - params.value);
     }
 
     function testSudoswapBuyItems() public {
@@ -189,7 +189,7 @@ contract SwapGenieCallParametersTest is Test, Interop, DeployRouter {
 
         require(success, "call failed");
         assertEq(token.balanceOf(RECIPIENT), 3);
-        assertEq(from.balance, balance-params.value);
+        assertEq(from.balance, balance - params.value);
     }
 
     function testPartialFill() public {
