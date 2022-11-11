09a2faffc61de740ca205e7f0e4df096aec95af6
Pote <will@uniswap.org>
2022-11-10 19:18:10 -0500

    Update addresses and block

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 // TODO: update with narwhal addrses when deployed
 // or else we should use placeholder for self recipient as in v3
-export const NARWHAL_ADDRESS = '0xE808C1cfeebb6cb36B537B82FA7c9EEf31415a05'
-export const PERMIT2_ADDRESS = '0x4a873bdd49f7f9cc0a5458416a12973fab208f8d'
+export const NARWHAL_ADDRESS = '0x412904b32001D2ea139B83d022D4173f720BAde5'
+export const PERMIT2_ADDRESS = '0x6fEe9BeC3B3fc8f9DA5740f0efc6BbE6966cd6A6'
 
 export const CONTRACT_BALANCE = 0
 export const ETH_ADDRESS = '0x0000000000000000000000000000000000000000'
