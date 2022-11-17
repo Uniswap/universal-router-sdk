5e50962209bee53e500aea1cb930ef23039e62dd
Pote <will@uniswap.org>
2022-11-17 13:24:40 -0500

    Update addresses

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,21 @@
 import { BigNumber } from 'ethers'
 
-export const ROUTER_ADDRESS = '0x5393904db506415D941726f3Cf0404Bb167537A0'
+export const UNIVERSAL_ROUTER_ADDRESS = (chainId: number): string => {
+  switch(chainId) {
+    case 1:
+      return '0x0000000052BE00bA3a005edbE83a0fB9aaDB964C'
+    case 137:
+      return '0xcedf31643c19f57b59608fae04202658310a895b'
+    case 10:
+      return '0x7166e00AbfaaB56Ca83b0025a9157FeBb01D31b3'
+    case 42161:
+      return '0xb36D7488EF109153679C1785aCcD1314549313E5'
+    case 42220:
+      return '0x2711385d4d6BfEd6B3EC06CeE352056a27A3B753'
+    default:
+      throw new Error(`Universal Router not deployed on chain ${chainId}`)
+  }
+}
 export const PERMIT2_ADDRESS = '0x6fEe9BeC3B3fc8f9DA5740f0efc6BbE6966cd6A6'
 
 export const CONTRACT_BALANCE = BigNumber.from(2).pow(255)
