b618e778ab82c09351a9c66e4d3a8675517cbe9c
Emily Williams <emag3m@gmail.com>
2022-12-09 15:57:49 -0500

    fix solidity parsing

--- tests/forge/writeInterop.ts
+++ tests/forge/writeInterop.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import { hexToDecimalString } from '../utils/hexToDecimalString'
 
 const INTEROP_FILE = './tests/forge/interop.json'
 
@@ -8,6 +9,7 @@ export function registerFixture(key: string, data: any) {
     ? JSON.parse(fs.readFileSync(INTEROP_FILE).toString())
     : {}
 
+  data.value = hexToDecimalString(data.value)
   interop[key] = data
   fs.writeFileSync(INTEROP_FILE, JSON.stringify(interop, null, 2))
 }
