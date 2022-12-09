4b5d98f35e123ba5352245263862ec8cd65c93d6
Emily Williams <emag3m@gmail.com>
2022-12-09 15:43:54 -0500

    return hex values

---
+++ src/utils/toHex.ts
@@ -0,0 +1,13 @@
+// /**
+//  * Converts a big int to a hex string
+//  * @param bigintIsh
+//  * @returns The hex encoded calldata
+//  */
+// export function toHex(bigNumber: BigNumber) {
+//   const bigInt = JSBI.BigInt(bigintIsh)
+//   let hex = bigInt.toString(16)
+//   if (hex.length % 2 !== 0) {
+//     hex = `0${hex}`
+//   }
+//   return `0x${hex}`
+// }
