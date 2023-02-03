c926b408149a40bfe81793c6f236772e0fac40a6
Alice Henshaw <henshawalice@gmail.com>
2023-02-03 17:34:39 +0100

    deployment addresses and bump package

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,16 +8,20 @@ export const UNIVERSAL_ROUTER_ADDRESS = (chainId: number): string => {
       return '0x4648a43B2C14Da09FdF82B161150d3F634f40491'
     case 137: // polygon
       return '0x4C60051384bd2d3C01bfc845Cf5F4b44bcbE9de5'
+    case 80001: // polygon mumbai
+      return '0x4648a43B2C14Da09FdF82B161150d3F634f40491'
     case 10: // optimism
       return '0xb555edF5dcF85f42cEeF1f3630a52A108E55A654'
-    case 42161: // arbitrum
-      return '0x4C60051384bd2d3C01bfc845Cf5F4b44bcbE9de5'
     case 420: // optimism goerli
       return '0x4648a43B2C14Da09FdF82B161150d3F634f40491'
+    case 42161: // arbitrum
+      return '0x4C60051384bd2d3C01bfc845Cf5F4b44bcbE9de5'
     case 421613: // arbitrum goerli
       return '0x4648a43B2C14Da09FdF82B161150d3F634f40491'
     case 42220: // celo
       return '0xC73d61d192FB994157168Fb56730FdEc64C9Cb8F'
+    case 44787: // celo alfajores
+      return '0x4648a43B2C14Da09FdF82B161150d3F634f40491'
     default:
       throw new Error(`Universal Router not deployed on chain ${chainId}`)
   }
@@ -31,12 +35,14 @@ export const WETH_ADDRESS = (chainId: number): string => {
       return '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'
     case 137: // polygon
       return '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
+    case 80001: // polygon mumbai
+      return '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889'
     case 10: // optimism
+      return '0x4200000000000000000000000000000000000006'
+    case 420: // optimism goerli
       return '0x4200000000000000000000000000000000000006'
     case 42161: // arbitrum
       return '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
-    case 420: // optimism goerli
-      return '0x4200000000000000000000000000000000000006'
     case 421613: // arbitrum goerli
       return '0xe39Ab88f8A4777030A534146A9Ca3B52bd5D43A3'
     default:
