f0d59f57d66ea5f40999d9ccb42440dd4375e324
marktoda <toda.mark@gmail.com>
2023-02-14 09:16:12 -0800

    fix: signature sanitization (#102)
    * fix: signature sanitization
    
    This commit switches to only sanitizing signatures of normal ECDSA
    length (64 or 65 bytes). Other signatures may be eip1271 related so we
    leave them be
    
    * fix: comments

--- src/utils/permit2.ts
+++ src/utils/permit2.ts
@@ -6,8 +6,18 @@ export interface Permit2Permit extends PermitSingle {
   signature: string
 }
 
+const SIGNATURE_LENGTH = 65
+const EIP_2098_SIGNATURE_LENGTH = 64
+
 export function encodePermit(planner: RoutePlanner, permit: Permit2Permit): void {
-  // sanitizes signature to cover edge cases like malformed EIP-2098 sigs and v used as recovery id
-  const sanitizedSignature = ethers.utils.joinSignature(ethers.utils.splitSignature(permit.signature))
-  planner.addCommand(CommandType.PERMIT, [permit, sanitizedSignature])
+  let signature = permit.signature
+
+  const length = ethers.utils.arrayify(permit.signature).length
+  // signature data provided for EIP-1271 may have length different from ECDSA signature
+  if (length === SIGNATURE_LENGTH || length === EIP_2098_SIGNATURE_LENGTH) {
+    // sanitizes signature to cover edge cases of malformed EIP-2098 sigs and v used as recovery id
+    signature = ethers.utils.joinSignature(ethers.utils.splitSignature(permit.signature))
+  }
+
+  planner.addCommand(CommandType.PERMIT, [permit, signature])
 }
