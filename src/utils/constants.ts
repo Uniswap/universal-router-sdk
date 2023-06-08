ae002d89879c54e1155658b404a8be36bb530764
hensha256 <henshawalice@gmail.com>
2023-06-08 17:43:40 +0100

    single mapping

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,90 +1,104 @@
 import { BigNumber } from 'ethers'
 
+type ChainConfig = {
+  router: string
+  creationBlock: number
+  weth: string
+}
+
+const WETH_NOT_SUPPORTED_ON_CHAIN = '0x0000000000000000000000000000000000000000'
+
+const CHAIN_CONFIGS: { [key: number]: ChainConfig } = {
+  // mainnet
+  [1]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    creationBlock: 17143817,
+  },
+  // goerli
+  [5]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+    creationBlock: 8940568,
+  },
+  // sepolia
+  [11155111]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14',
+    creationBlock: 3543575,
+  },
+  // polygon
+  [137]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+    creationBlock: 42294741,
+  },
+  //polygon mumbai
+  [80001]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889',
+    creationBlock: 35176052,
+  },
+  //optimism
+  [10]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: '0x4200000000000000000000000000000000000006',
+    creationBlock: 96333990,
+  },
+  // optimism goerli
+  [420]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: '0x4200000000000000000000000000000000000006',
+    creationBlock: 8887728,
+  },
+  // arbitrum
+  [42161]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+    creationBlock: 87206402,
+  },
+  // arbitrum goerli
+  [421613]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: '0xe39Ab88f8A4777030A534146A9Ca3B52bd5D43A3',
+    creationBlock: 18815277,
+  },
+  // celo
+  [42220]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: WETH_NOT_SUPPORTED_ON_CHAIN,
+    creationBlock: 19106929,
+  },
+  // celo alfajores
+  [44787]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: WETH_NOT_SUPPORTED_ON_CHAIN,
+    creationBlock: 17566658,
+  },
+  // binance smart chain
+  [56]: {
+    router: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+    weth: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    creationBlock: 27915533,
+  },
+}
+
 export const UNIVERSAL_ROUTER_ADDRESS = (chainId: number): string => {
-  switch (chainId) {
-    case 1: // mainnet
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 5: // goerli
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 11155111: // sepolia
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 137: // polygon
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 80001: // polygon mumbai
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 10: // optimism
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 420: // optimism goerli
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 42161: // arbitrum
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 421613: // arbitrum goerli
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 42220: // celo
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 44787: // celo alfajores
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    case 56: // binance smart chain
-      return '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'
-    default:
-      throw new Error(`Universal Router not deployed on chain ${chainId}`)
-  }
+  if (!(chainId in CHAIN_CONFIGS)) throw new Error(`Universal Router not deployed on chain ${chainId}`)
+  return CHAIN_CONFIGS[chainId].router
 }
 
 export const UNIVERSAL_ROUTER_CREATION_BLOCK = (chainId: number): number => {
-  switch (chainId) {
-    case 1: // mainnet
-      return 17143817
-    case 5: // goerli
-      return 8940568
-    case 11155111: // sepolia
-      return 3543575
-    case 137: // polygon
-      return 42294741
-    case 80001: // polygon mumbai
-      return 35176052
-    case 10: // optimism
-      return 96333990
-    case 420: // optimism goerli
-      return 8887728
-    case 42161: // arbitrum
-      return 87206402
-    case 421613: // arbitrum goerli
-      return 18815277
-    case 42220: // celo
-      return 19106929
-    case 44787: // celo alfajores
-      return 17566658
-    case 56: // binance smart chain
-      return 27915533
-    default:
-      throw new Error(`Universal Router not deployed on chain ${chainId}`)
-  }
+  if (!(chainId in CHAIN_CONFIGS)) throw new Error(`Universal Router not deployed on chain ${chainId}`)
+  return CHAIN_CONFIGS[chainId].creationBlock
 }
 
 export const WETH_ADDRESS = (chainId: number): string => {
-  switch (chainId) {
-    case 1: //mainnet
-      return '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-    case 5: // goerli
-      return '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'
-    case 137: // polygon
-      return '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
-    case 80001: // polygon mumbai
-      return '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889'
-    case 10: // optimism
-      return '0x4200000000000000000000000000000000000006'
-    case 420: // optimism goerli
-      return '0x4200000000000000000000000000000000000006'
-    case 42161: // arbitrum
-      return '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
-    case 421613: // arbitrum goerli
-      return '0xe39Ab88f8A4777030A534146A9Ca3B52bd5D43A3'
-    case 56: // binance smart chain
-      return '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
-    default:
-      throw new Error(`WETH9 or UniversalRouter not deployed on chain ${chainId}`)
-  }
+  if (!(chainId in CHAIN_CONFIGS)) throw new Error(`Universal Router not deployed on chain ${chainId}`)
+
+  if (CHAIN_CONFIGS[chainId].weth == WETH_NOT_SUPPORTED_ON_CHAIN) throw new Error(`Chain ${chainId} does not have WETH`)
+
+  return CHAIN_CONFIGS[chainId].weth
 }
 
 export const PERMIT2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
