2ba524394cfc9ee8a513dd7a75136f2a477ea310
Charles Bachmeier <charles@bachmeier.io>
2023-07-24 11:03:48 -0700

    feat: export UNIVERSAL_ROUTER_CREATION_BLOCK (#139)
    * feat: export UNIVERSAL_ROUTER_CREATION_BLOCK
    
    * fix: prettier

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export { SwapRouter } from './swapRouter'
 export * from './entities'
-export { UNIVERSAL_ROUTER_ADDRESS, PERMIT2_ADDRESS, ROUTER_AS_RECIPIENT, WETH_ADDRESS } from './utils/constants'
+export {
+  UNIVERSAL_ROUTER_ADDRESS,
+  UNIVERSAL_ROUTER_CREATION_BLOCK,
+  PERMIT2_ADDRESS,
+  ROUTER_AS_RECIPIENT,
+  WETH_ADDRESS,
+} from './utils/constants'
